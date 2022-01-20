016e34b27ffd703ba1090919f89ddfdc6d9e5e9f
artiusm <margo.artiushenko@autodesk.com>
2022-01-20 13:20:13 -0800

    Change text.

--- js/footer.jsx
+++ js/footer.jsx
@@ -18,7 +18,7 @@ var app = app || {};
 					<button
 						className="clear-completed"
 						onClick={this.props.onClearCompleted}>
-						!!!Clear completed
+						!!!Clear completed!!!
 					</button>
 				);
 			}
